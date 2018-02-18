3b1149b94f46a00d046496f371eba1730bb2dd79
Samuel Atkins <sjatkins12@gmail.com>
2018-02-17 19:14:29 -0800

    add instruct len

--- vm/includes/vm.h
+++ vm/includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: satkins <satkins@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/12 21:49:26 by satkins           #+#    #+#             */
-/*   Updated: 2018/02/17 14:44:09 by satkins          ###   ########.fr       */
+/*   Updated: 2018/02/17 17:17:02 by satkins          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,10 @@
 
 # define OPCODE_OFFSET 56;
 
-typedef struct  s_reg
-{
-	char        reg[ REG_SIZE ];
-}               t_reg;
-
 typedef struct  s_process
 {
 	void		*pc;
-	t_reg		regs[ REG_NUMBER ];
+	char		regs[ REG_NUMBER ][ REG_SIZE ];
 	int			execute_cycle;
 	int			carry : 1;
 	int			process_num;
