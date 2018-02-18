b8b839b538aa050e76f2543161695a85432b16fe
Philippe Asf <assefphilippe@gmail.com>
2018-02-18 15:23:01 -0800

    s maj

--- vm/op/live.c
+++ vm/op/live.c
@@ -1,20 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ins_add.c                                          :+:      :+:    :+:   */
+/*   live.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: passef <passef@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/17 16:59:40 by passef            #+#    #+#             */
-/*   Updated: 2018/02/17 19:15:10 by passef           ###   ########.fr       */
+/*   Updated: 2018/02/18 15:06:18 by passef           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-void    ins_add(t_process *pc)
+void    op_live(t_process *pc,  t_arena *arena)
 {
-    int x;
-    int y;
-    int res;
+    int player_id;
+    int player_pos;
+
+    
+    if (player_pos > -1)
+    {
+        t_arena->lives += 1;
+        t_arena->winner = player_pos;
+    }
 }
