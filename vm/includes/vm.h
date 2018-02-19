4cc2d5f79743bc3c58cbbd30ec9714d50f8f174d
Samuel Atkins <sjatkins12@gmail.com>
2018-02-18 19:29:06 -0800

    fixxed_struct

--- vm/includes/vm.h
+++ vm/includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: satkins <satkins@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/12 21:49:26 by satkins           #+#    #+#             */
-/*   Updated: 2018/02/18 19:11:53 by satkins          ###   ########.fr       */
+/*   Updated: 2018/02/18 19:28:55 by satkins          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 # include "libft.h"
 # include "op.h"
+
+typedef struct		s_instruction
+{
+	int				op_code;
+	unsigned char	*pc;
+	unsigned char	*param[ MAX_PARAM ];
+	int				param_size[ MAX_PARAM ];
+}					t_instruction;
 
 typedef struct		s_process
 {
@@ -25,14 +33,6 @@ typedef struct		s_process
 	int				player_num;
 	int				num_live;
 }					t_process;
-
-typedef struct		s_instruction
-{
-	int				op_code;
-	unsigned char	*pc;
-	unsigned char	*param[ MAX_PARAM ];
-	int				param_size[ MAX_PARAM ];
-}					t_instruction;
 
 typedef struct		s_player
 {
@@ -50,6 +50,7 @@ typedef struct		s_arena
 	int				cycle_to_die;
 	t_pqueue		*proc_queue;
 	t_player		*players;
+	int				last_alive;
 	unsigned char	arena[ MEM_SIZE ];
 }					t_arena;
 
