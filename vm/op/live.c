cc193065d14a80b64a90e0663cc001cfe8ca9cc9
Philippe Asf <assefphilippe@gmail.com>
2018-02-18 19:29:57 -0800

    Merge branch 'pa_dev_vm'

--- vm/op/live.c
+++ vm/op/live.c
@@ -6,16 +6,29 @@
 /*   By: passef <passef@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/17 16:59:40 by passef            #+#    #+#             */
-/*   Updated: 2018/02/18 19:04:42 by passef           ###   ########.fr       */
+/*   Updated: 2018/02/18 19:20:31 by passef           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-void    op_live(t_process *pc,  t_arena *arena)
+int         get_data(t_instruction *ins, t_arena *arena)
+{
+    if (ins->op_code == 1)
+        op_live(ins->param[0], arena);
+}
+
+int       get_player_id(int n)
+{
+    
+}
+
+void    op_live(unsigned char *player_num, t_arena *arena)
 {
     unsigned char *tmp;
+    int             player_id;
 
-    t_process->pc;
+    player_id = get_player_id(player_num);
+    pc = arena->players->player_num;
 
 }
