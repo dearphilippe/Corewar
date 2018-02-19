53592d8b44484b34137a895e4678cf4eb788f229
Nicholas KOURIS <nkouris@student.42.us.org>
2018-02-18 20:26:00 -0800

    main fleshing, testing for endianness

--- assembler/main.c
+++ assembler/main.c
@@ -6,18 +6,22 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/18 13:27:53 by nkouris           #+#    #+#             */
-/*   Updated: 2018/02/18 17:21:48 by nkouris          ###   ########.fr       */
+/*   Updated: 2018/02/18 20:21:58 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "op.h"
 
+void	read_sfile()
+{
+}
+
 void	usage_warning()
 {
 	ft_printf("Usage: ./asm [file ...]\n");
-	errno = ERR_USAGE;
-
+	errno = USAGE_ERR;
+	exit (errno);
 }
 
 int		main(int argc, char **argv)
--- sample/vm_champs/test.c
+++ sample/vm_champs/test.c
@@ -1,8 +1,25 @@
-#!/bin/sh
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
-a=1
+int main()
+{
+    int 	fd = open("champs/ex.cor", O_RDONLY);
+    char    buff[4];
+    int 	result;
+    int 	*value;
 
-while [ "$a" ];
-do
-	./corewar champs/Gagnant.cor champs/maxidef.cor
-done
+    read(fd, buff, 4);
+    result = 0;
+	printf("leftmost: %#x\nbyte: %#x\nbyte: %#x\nrightmost: %#x\n", buff[0], buff[1], buff[2], buff[3]);
+	value = (int *)buff;
+	printf("cast: %#x\n", *value);
+    result |= (*value & 0x000000FF) << 24;
+    result |= (*value & 0x0000FF00) << 8;
+    result |= (*value & 0x00FF0000) >> 8;
+    result |= (*value & 0xFF000000) >> 24;
+    printf("read : %d\n", *value == 0xea8373);
+    printf("result : %d\n", *value == 0xea8373);
+    printf("read: %#x\nresult: %#x\nmagic: %#x\n", *value, result, 15369075); 
+	return (0);
+}
