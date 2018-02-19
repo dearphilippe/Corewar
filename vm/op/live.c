980e5eacc272f84cbc63816b322c8f6a25659c80
Philippe Asf <assefphilippe@gmail.com>
2018-02-18 19:08:59 -0800

    Auto stash before merge of "master" and "dev_vm"

--- vm/op/live.c
+++ vm/op/live.c
@@ -6,7 +6,7 @@
 /*   By: passef <passef@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/17 16:59:40 by passef            #+#    #+#             */
-/*   Updated: 2018/02/18 15:06:18 by passef           ###   ########.fr       */
+/*   Updated: 2018/02/18 19:04:42 by passef           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,8 @@
 
 void    op_live(t_process *pc,  t_arena *arena)
 {
-    int player_id;
-    int player_pos;
+    unsigned char *tmp;
 
-    
-    if (player_pos > -1)
-    {
-        t_arena->lives += 1;
-        t_arena->winner = player_pos;
-    }
+    t_process->pc;
+
 }
